e2a3c5b306ecf4858f67f192d69a956761a5169a
Muhammad <101938060+RR77R@users.noreply.github.com>
2022-05-29 04:56:14 +0300

    Update __init__.py

--- jmthon/clients/__init__.py
+++ jmthon/clients/__init__.py
@@ -1,4 +1,4 @@
 from .client_list import clients_list, client_id
 from .decs import jmthon_cmd, jmthon_handler
-from .session import Jmthon, H2, H3, H4, H5, RJMTHON
+from .session import SBB_B, H2, H3, H4, H5, RJMTHON
 # jmthon
