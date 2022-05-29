66e64ea3f593fbb2e27472f6727a4fe3500ba527
Muhammad <101938060+RR77R@users.noreply.github.com>
2022-05-29 03:30:36 +0300

    Add files via upload

--- jmthon/__init__.py
+++ jmthon/__init__.py
@@ -1,1 +1,82 @@
+import heroku3
+import os
+import sys
+import time
 
+from distutils.util import strtobool as sb
+from logging import DEBUG, INFO, basicConfig, getLogger
+
+from jmthon.clients.session import H2, H3, H4, H5, Jmthon, RJMTHON
+from jmthon.config import Config
+
+
+StartTime = time.time()
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+
+if CONSOLE_LOGGER_VERBOSE:
+    basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=DEBUG,
+    )
+else:
+    basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                level=INFO)
+
+
+LOGS = getLogger(__name__)
+
+bot = Jmthon
+tbot = RJMTHON
+
+
+if not Config.API_HASH:
+    LOGS.warning(" تأكد من فار الايبي هاش اولا ")
+    quit(1)
+
+
+if not Config.APP_ID:
+    LOGS.warning("تاكد من فار الايبي ايدي اولا")
+    quit(1)
+
+
+if not Config.BOT_TOKEN:
+    LOGS.warning("تاكد من فار توكن البوت اولت")
+    quit(1)    
+
+if not Config.DB_URI:    
+    LOGS.warning("تأكد من فار قاعده البيانات الخاصه بك اولا.")
+    quit(1)
+
+
+if not Config.JMTHON_SESSION:
+    LOGS.warning("تاكد من فار سيشن جمثون اولا.")
+    quit(1)
+
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+
+CMD_LIST = {}
+CMD_HELP = {}
+CMD_HELP_BOT = {}
+CMD_INFO = {}
+INT_PLUG = ""
+LOAD_PLUG = {}
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+ISAFK = False
+AFKREASON = None
+SUDO_LIST = {}
+
+#t.me/JMTHON
---
+++ jmthon/__main__.py
@@ -0,0 +1,91 @@
+import glob
+import os
+import sys
+
+from pathlib import Path
+from telethon import Button, TelegramClient
+from telethon.utils import get_peer_id
+
+from jmthon import LOGS, bot, tbot
+from jmthon.clients.session import Jmthon, H2, H3, H4, H5
+from jmthon.config import Config
+from jmthon.utils import join_it, load_module, logger_check, start_msg, update_sudo, plug_channel
+from jmthon.version import __jmthon__ as jmthonver
+
+rz = Config.HANDLER
+
+JMTHON_PIC = "https://telegra.ph/file/d85083f6bb31ec14d912b.jpg"
+
+
+async def jmthons(session=None, client=None, session_name="Main"):
+    if session:
+        LOGS.info(f"••• يتم تشغيل الحسابات [{session_name}] •••")
+        try:
+            await client.start()
+            return 1
+        except:
+            LOGS.error(f" خطأ في {session_name} تأكد وحاول مجددا")
+            return 0
+    else:
+        return 0
+
+
+async def plug_load(path):
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as jmthon:
+            path1 = Path(jmthon.name)
+            shortname = path1.stem
+            if shortname.replace(".py", "") in Config.UNLOAD:
+                os.remove(Path(f"jmthon/plugins/{shortname}.py"))
+            else:
+                load_module(shortname.replace(".py", ""))      
+
+
+async def jmthon_is_on(total):
+    await update_sudo()
+    await logger_check(bot)
+    await start_msg(tbot, JMTHON_PIC, jmthonver, total)
+    await join_it(bot)
+    await join_it(H2)
+    await join_it(H3)
+    await join_it(H4)
+    await join_it(H5)
+
+
+async def start_jmthon():
+    try:
+        tbot_id = await tbot.get_me()
+        Config.BOT_USERNAME = f"@{tbot_id.username}"
+        bot.tgbot = tbot
+        LOGS.info("••• يتم تشغيل جمثون •••")
+        C1 = await jmthons(Config.JMTHON_SESSION, bot, "JMTHON_SESSION")
+        C2 = await jmthons(Config.SESSION_2, H2, "SESSION_2")
+        C3 = await jmthons(Config.SESSION_3, H3, "SESSION_3")
+        C4 = await jmthons(Config.SESSION_4, H4, "SESSION_4")
+        C5 = await jmthons(Config.SESSION_5, H5, "SESSION_5")
+        await tbot.start()
+        total = C1 + C2 + C3 + C4 + C5
+        LOGS.info("••• اكتملت عمليه التنصيب •••")
+        LOGS.info("••• يتم بدء تنزيل الملفات •••")
+        await plug_load("jmthon/plugins/*.py")
+        await plug_channel(bot, Config.PLUGIN_CHANNEL)
+        LOGS.info("• تم تشغيل سورس جمثون بنجاح  •")
+        LOGS.info("للحصول على اخر اخبار و التحديثات الخاصه بالسورس انضم الى القناه  @jmthon")
+        LOGS.info(f"» مجموع الحسابات = {str(total)} «")
+        await jmthon_is_on(total)
+    except Exception as e:
+        LOGS.error(f"{str(e)}")
+        sys.exit()
+
+
+bot.loop.run_until_complete(start_jmthon())
+
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    try:
+        bot.run_until_disconnected()
+    except ConnectionError:
+        pass
+
