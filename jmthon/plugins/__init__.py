b86b117a9633a93dda88f7e9429624a47b03092d
Muhammad <101938060+RR77R@users.noreply.github.com>
2022-05-29 03:53:48 +0300

    Add files via upload

--- jmthon/plugins/__init__.py
+++ jmthon/plugins/__init__.py
@@ -1,1 +1,46 @@
+import datetime
+import time
 
+from jmthon import *
+from jmthon.clients import *
+from jmthon.config import Config
+from jmthon.helpers import *
+from jmthon.utils import *
+from jmthon.random_strings import *
+from jmthon.version import __jmthon__
+from jmthon.sql.gvar_sql import gvarstat
+from telethon import version
+
+jmthon_logo = "./jmthon/resources/pics/jmthon_logo.jpg"
+hl = Config.HANDLER
+shl = Config.SUDO_HANDLER
+jmthon_ver = __jmthon__
+tel_ver = version.__version__
+
+async def get_user_id(ids):
+    if str(ids).isdigit():
+        userid = int(ids)
+    else:
+        userid = (await bot.get_entity(ids)).id
+    return userid
+
+is_sudo = "True" if gvarstat("SUDO_USERS") else "False"
+
+abus = Config.ABUSE
+if abus == "ON":
+    abuse_m = "Enabled"
+else:
+    abuse_m ="Disabled"
+
+
+my_channel = Config.MY_CHANNEL or "JMTHON"
+my_group = Config.MY_GROUP or "JMTHON_support"
+if "@" in my_channel:
+    my_channel = my_channel.replace("@", "")
+if "@" in my_group:
+    my_group = my_group.replace("@", "")
+
+chnl_link = "https://t.me/JMTHON"
+jmthon_channel = f"[قناة السورس]({chnl_link})"
+grp_link = "https://t.me/JMTHON_support"
+jmthon_grp = f"[مجموعة السورس]({grp_link})"
---
+++ jmthon/plugins/الفحص.py
@@ -0,0 +1,79 @@
+import datetime
+import random
+import time
+
+from telethon.errors import ChatSendInlineForbiddenError as noin
+from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot
+
+from jmthon.sql.gvar_sql import gvarstat
+from . import *
+
+#-------------------------------------------------------------------------------
+
+ALIVE_TEMP = """
+<b><i>• سورس جمثون يعمل بنجاح •</b></i>
+<i><b>↼ المالك ⇀</i></b> : 『 <a href='tg://user?id={}'>{}</a> 』
+╭──────────────
+┣─ <b>» التيليثون ~</b> <i>{}</i>
+┣─ <b>» جمثون ~</b> <i>{}</i>
+┣─ <b>» الوقت ~</b> <i>{}</i>
+┣─ <b>» البنك ~</b> <i>{}</i>
+╰──────────────
+<b><i>»»» <a href='https://t.me/jmthon'>[ ثناة السورس ]</a> «««</i></b>
+"""
+
+msg = """{}\n
+<b><i>• حالة البوت / •</b></i>
+<b>التيليثون ≈</b>  <i>{}</i>
+<b>جمثون ≈</b>  <i>{}</i>
+<b>الوقت ≈</b>  <i>{}</i>
+"""
+#-------------------------------------------------------------------------------
+
+@jmthon_cmd(pattern=$")
+async def up(event):
+    cid = await client_id(event)
+    RAZAN, JMTHON_USER, jmthon_mention = cid[0], cid[1], cid[2]
+    start = datetime.datetime.now()
+    jmthon = await eor(event, "- أنتظر قليلا")
+    uptime = await get_time((time.time() - StartTime))
+    a = gvarstat("ALIVE_PIC")
+    pic_list = []
+    if a:
+        b = a.split(" ")
+        if len(b) >= 1:
+            for c in b:
+                pic_list.append(c)
+        PIC = random.choice(pic_list)
+    else:
+        PIC = "https://telegra.ph/file/d85083f6bb31ec14d912b.jpg"
+    end = datetime.datetime.now()
+    ling = (end - start).microseconds / 1000
+    omk = ALIVE_TEMP.format(RAZAN, JMTHON_USER, tel_ver, jmthon_ver, uptime, ling)
+    await event.client.send_file(event.chat_id, file=PIC, caption=omk, parse_mode="HTML")
+    await jmthon.delete()
+
+
+
+@jmthon_cmd(pattern="جمثون$")
+async def jmthon_a(event):
+    cid = await client_id(event)
+    RAZAN, JMTHON_USER, jmthon_mention = cid[0], cid[1], cid[2]
+    uptime = await get_time((time.time() - StartTime))
+    am = gvarstat("ALIVE_MSG") or "<b>»» سورس جمثون يعمل بنجاح ««</b>"
+    try:
+        jmthon = await event.client.inline_query(Config.BOT_USERNAME, "فحص")
+        await jmthon[0].click(event.chat_id)
+        if event.sender_id == RAZAN:
+            await event.delete()
+    except (noin, dedbot):
+        await eor(event, msg.format(am, tel_ver, jmthon_ver, uptime), parse_mode="HTML")
+
+
+CmdHelp("الفحص").add_command(
+  "فحص", None, "امر فحص جربه بنفسك اكتب الامر فقط"
+).add_command(
+  "جمثون", None, "امر فحص بوضع الانلاين جربه بنفسك."
+).add_warning(
+  "لا توجد مخاطر ✅"
+).add()
