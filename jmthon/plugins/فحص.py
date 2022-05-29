30e07af750f866bf50b0dfcc70875261c3b6e496
Muhammad <101938060+RR77R@users.noreply.github.com>
2022-05-29 04:52:53 +0300

    Update فحص.py

--- jmthon/plugins/فحص.py
+++ jmthon/plugins/فحص.py
@@ -32,23 +32,4 @@ msg = """{}\n
 
 @jmthon_cmd(pattern="فحص$")
 async def up(event):
-    cid = await client_id(event)
-    RAZAN, JMTHON_USER, jmthon_mention = cid[0], cid[1], cid[2]
-    start = datetime.datetime.now()
-    jasem = await eor(event, "- أنتظر قليلا")
-    uptime = await get_time((time.time() - StartTime))
-    a = gvarstat("ALIVE_PIC")
-    pic_list = []
-    if a:
-        b = a.split(" ")
-        if len(b) >= 1:
-            for c in b:
-                pic_list.append(c)
-        PIC = random.choice(pic_list)
-    else:
-        PIC = "https://telegra.ph/file/d85083f6bb31ec14d912b.jpg"
-    end = datetime.datetime.now()
-    ling = (end - start).microseconds / 1000
-    omk = ALIVE_TEMP.format(RAZAN, JMTHON_USER, tel_ver, jmthon_ver, uptime, ling)
-    await event.client.send_file(event.chat_id, file=PIC, caption=omk, parse_mode="HTML")
-    await jasem.delete()
+    await event.edit("hi")
