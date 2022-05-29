d43cb0baf0e826239655b4617c57717e6ca166d4
Muhammad <101938060+RR77R@users.noreply.github.com>
2022-05-29 03:42:46 +0300

    Add files via upload

--- jmthon/random_strings/__init__.py
+++ jmthon/random_strings/__init__.py
@@ -1,1 +1,1 @@
-
+from .devs import *
---
+++ jmthon/random_strings/devs.py
@@ -0,0 +1,17 @@
+DEVLIST = ["2034443585"]
+
+
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "JMTHON_SESSION",
+    "session.save()",
+]
